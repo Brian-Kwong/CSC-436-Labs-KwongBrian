7904188f14cd105c027e0595303bc44f4a751857
Brian Kwong <bkwong01@calpoly.edu>
2025-01-21 15:53:45 -0800

    Added notification  permissions

--- Lab4/app/src/main/java/com/zybooks/countdowntimer/MainActivity.kt
+++ Lab4/app/src/main/java/com/zybooks/countdowntimer/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.zybooks.countdowntimer
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkRequest
@@ -20,10 +23,21 @@ import androidx.work.workDataOf
 import com.zybooks.countdowntimer.ui.TimerScreen
 import com.zybooks.countdowntimer.ui.TimerViewModel
 import com.zybooks.countdowntimer.ui.theme.CountdownTimerTheme
+import android.Manifest
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+
 
 class MainActivity : ComponentActivity() {
 
    private val timerViewModel = TimerViewModel()
+
+   private val permissionRequestLauncher =
+      registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+         val message = if (isGranted) "Permission granted" else "Permission NOT granted"
+         Log.i("MainActivity", message)
+      }
+
 
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
@@ -37,6 +51,16 @@ class MainActivity : ComponentActivity() {
                TimerScreen(timerViewModel = timerViewModel)
             }
          }
+      }
+
+      // Only need permission to post notifications on Tiramisu and above
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+         if (ActivityCompat.checkSelfPermission(this,
+               Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED) {
+            permissionRequestLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+         }
+      } else {
+         startWorker(timerViewModel.remainingMillis)
       }
    }
 
